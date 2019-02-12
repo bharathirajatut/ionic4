7486428ef881e19f87c83d13f019472211f171e8
Bharathiraja <bharathirajatut@gmail.com>
2019-02-12 10:35:36 +0530

    Camera Example
    Camera Example using Ionic 4

---
+++ camera-example/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private camera: Camera){}
+
+  image:any=''
+  openCam(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     //alert(imageData)
+     this.image=(<any>window).Ionic.WebView.convertFileSrc(imageData);
+    }, (err) => {
+     // Handle error
+     alert("error "+JSON.stringify(err))
+    });
+
+  }
+
+}
