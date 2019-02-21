32f25f9e6edd3fb6076ac2dabdf3fab1e50b387e
Bharathiraja <bharathirajatut@gmail.com>
2019-02-21 18:30:03 +0530

    tabs example
    tabs example

---
+++ tabs-example/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './home/home.module#HomePageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
