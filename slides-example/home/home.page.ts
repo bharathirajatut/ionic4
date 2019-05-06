ce84bdd6dff2bddf517816e3055e4a91599d9c0c
Bharathiraja <bharathirajatut@gmail.com>
2019-05-06 12:51:27 +0530

    slides example

---
+++ slides-example/home/home.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  slides = [
+    {
+      title: "Welcome to the Docs!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "https://ionicframework.com/docs/v3/dist/preview-app/www/assets/img/ica-slidebox-img-1.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "https://ionicframework.com/docs/v3/dist/preview-app/www/assets/img/ica-slidebox-img-2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "https://ionicframework.com/docs/v3/dist/preview-app/www/assets/img/ica-slidebox-img-3.png",
+    }
+  ];
+}
